9d72ce38553fa8f33a08205812fea2963e56083d
Madhusudan N <madhu1992blue@outlook.com>
2024-12-28 14:07:53 +0000

    feat: determine block type

--- src/markdown.py
+++ src/markdown.py
@@ -1,4 +1,13 @@
 import re
+from enum import Enum
+class BlockType(Enum):
+    Heading = "heading"
+    Paragraph = "paragraph"
+    Code = "code"
+    Quote = "quote"
+    OrderedList = "orderedlist"
+    UnorderedList = "unorderedlist"
+
 def extract_markdown_images(text):
     return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)",text)
 
@@ -15,3 +24,53 @@ def markdown_to_blocks(markdown: str) -> list[str]:
             res.append(trimmedBlock)
     return res
     
+def block_to_block_type(block: str)->BlockType:
+    lines = block.split("\n")
+    if len(lines)==1 and block.startswith(("# ","## ","### ", "#### ", "##### ", "###### ")):
+        return BlockType.Heading
+    
+    if lines[0].startswith("```") and lines[-1].startswith("```"):
+        return BlockType.Code
+    
+    if lines[0].startswith(">"):
+        isQuote = True
+        for line in lines:
+            if not line.startswith(">"):
+                isQuote = False
+        if isQuote:
+            return BlockType.Quote
+        else:
+            return BlockType.Paragraph
+    
+    if lines[0].startswith("- "):
+        isList = True
+        for line in lines:
+            if not line.startswith("- "):
+                isList = False
+        if isList:
+            return BlockType.UnorderedList
+        else:
+            return BlockType.Paragraph
+    
+    if lines[0].startswith("* "):
+        isList = True
+        for line in lines:
+            if not line.startswith("* "):
+                isList = False
+        if isList:
+            return BlockType.UnorderedList
+        else:
+            return BlockType.Paragraph
+        
+    if lines[0].startswith("1. "):
+        isList = True
+        counter = 1
+        for line in lines:
+            if not line.startswith(f"{counter}. "):
+                isList = False
+            counter += 1
+        if isList:
+            return BlockType.OrderedList
+        else:
+            return BlockType.Paragraph
+    return BlockType.Paragraph
--- src/test_markdown.py
+++ src/test_markdown.py
@@ -1,5 +1,5 @@
 import unittest
-from markdown import extract_markdown_images, extract_markdown_links, markdown_to_blocks
+from markdown import extract_markdown_images, extract_markdown_links, markdown_to_blocks, block_to_block_type, BlockType
 
 class TestMarkdownParsing(unittest.TestCase):
     def test_image_extraction(self):
@@ -37,3 +37,50 @@ This is a paragraph of text. It has some **bold** and *italic* words inside of i
         ]
         self.assertListEqual(blocks, expected)
        
+    def test_block_type_detection(self):
+        ul = """
+* This is a ul
+* This is a ul1
+""".strip()
+        self.assertEqual(block_to_block_type(ul), BlockType.UnorderedList)
+
+        ul = """
+- This is a ul
+- This is a ul1
+""".strip()
+        self.assertEqual(block_to_block_type(ul), BlockType.UnorderedList)
+
+        malformed_ul = """
+- This is a ul
+- This is a ul1
+a
+""".strip()
+        self.assertEqual(block_to_block_type(malformed_ul), BlockType.Paragraph)
+
+        heading = "# This is a heading"
+        self.assertEqual(block_to_block_type(heading), BlockType.Heading)
+
+        malformed_heading = """# this is a heading
+        a"""
+        self.assertEqual(block_to_block_type(malformed_heading), BlockType.Paragraph)
+        
+        quote = """
+>Quote
+> quote
+""".strip()
+        self.assertEqual(block_to_block_type(quote), BlockType.Quote)
+
+
+        ol = """
+1. This is nice
+2. This is crazy
+""".strip()
+        self.assertEqual(block_to_block_type(ol), BlockType.OrderedList)
+
+        malformed_ol = """
+1. This is nice
+1. This is not so nice
+""".strip()
+        self.assertEqual(block_to_block_type(malformed_ol), BlockType.Paragraph)
+
+
